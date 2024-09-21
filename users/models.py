205c51885c1cd91c73bcaab3da938194dc93aff7
KPACABA1 <groninger.official@yandex.ru>
2024-09-21 15:56:33 +0500

    Создал модель платежа

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from materials.models import Course, Lesson
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -22,3 +24,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """Модель платежа"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='user')
+    date_of_payment = models.DateField(verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Оплаченный курс',
+                                    related_name='paid_course', null=True, blank=True)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='Оплаченный урок',
+                                    related_name='paid_lesson', null=True, blank=True)
+    payment_amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.BooleanField(default=True, verbose_name='Тип оплаты(если True - безнал, Если False - '
+                                                                    'наличка)')
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+
+    def __str__(self):
+        return f'Пользователь - {self.user}, оплатил {self.payment_amount}'
+
