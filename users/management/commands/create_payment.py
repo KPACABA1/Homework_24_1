08c1d02307f629135be676817cf5a0aa26c34804
KPACABA1 <groninger.official@yandex.ru>
2024-09-26 22:28:03 +0500

    Добавил, чтобы при использовании команды создавался пользователь, курс и урок дополнительно

--- users/management/commands/create_payment.py
+++ users/management/commands/create_payment.py
@@ -7,14 +7,23 @@ from users.models import Payment, User
 class Command(BaseCommand):
     """Команда для создания 2 платежей(1 платеж за курс, 2 за урок)"""
     def handle(self, *args, **options):
+        # создаю пользователя специально для команды
+        user = User.objects.create(email='test_command@yandex.ru')
+        user.set_password('12345')
+        user.save()
+
+        # Создаю урок и курс специально для команды
+        course = Course.objects.create(title='test_course', description='test_description')
+        lesson = Lesson.objects.create(title='test_lesson', description='test_description', course=course)
+
         # Создаю платеж за курс
-        payment_course = Payment.objects.create(user=User.objects.get(pk=1), date_of_payment='2024-09-21',
-                                                paid_course=Course.objects.get(pk=1), payment_amount=200000,
-                                                payment_method=True)
+        payment_course = Payment.objects.create(user=user, date_of_payment='2024-09-21',
+                                                paid_course=course, payment_amount=200000,
+                                                payment_method='cash')
         payment_course.save()
 
         # Создаю платеж за урок
-        payment_lesson = Payment.objects.create(user=User.objects.get(pk=1), date_of_payment='2024-09-21',
-                                                paid_lesson=Lesson.objects.get(pk=1), payment_amount=15000,
-                                                payment_method=True)
+        payment_lesson = Payment.objects.create(user=user, date_of_payment='2024-09-21',
+                                                paid_lesson=lesson, payment_amount=15000,
+                                                payment_method='non-cash')
         payment_lesson.save()
