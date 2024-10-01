cf9180bb4626c1ab14f8be89231ca90b3b27b313
KPACABA1 <groninger.official@yandex.ru>
2024-10-01 18:06:43 +0500

    Сделал для моделей курс и урок поле владелец

--- materials/models.py
+++ materials/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+from users.models import User
+
+
 # Create your models here.
 class Course(models.Model):
     """Модель курса"""
     title = models.CharField(max_length=35, verbose_name='название')
     preview = models.ImageField(verbose_name='Превью(изображение)', null=True, blank=True, upload_to='materials/course')
     description = models.TextField(verbose_name='Описание')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Создатель', related_name='user_course',
+                                null=True, blank=True)
 
     class Meta:
         verbose_name = 'Курс'
@@ -23,6 +28,8 @@ class Lesson(models.Model):
     link_to_video = models.TextField(verbose_name='Ссылка на видео', null=True, blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс',
                                     related_name='course', null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Создатель', related_name='user_lesson',
+                                null=True, blank=True)
 
     class Meta:
         verbose_name = 'Курс'
