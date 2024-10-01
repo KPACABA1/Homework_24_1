756bb39855900b5e999875f16e06836ea2b6cc15
KPACABA1 <groninger.official@yandex.ru>
2024-10-01 23:13:08 +0500

    Сделал фикстуру для групп и ввёл проверку на то, что пользователь модератор

--- materials/views.py
+++ materials/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer, CourseCreateSerializer
+from users.permissions import ModeratorPermission
 
 
 # Create your views here.
@@ -22,11 +23,22 @@ class CourseViewSet(ModelViewSet):
         course.creator = self.request.user
         course.save()
 
+    def get_permissions(self):
+        """Метод для распределения прав доступа"""
+        # Если действие на создание и удаление, то проверяем не является ли пользователь модератором
+        if self.action in ['create', 'destroy']:
+            self.permission_classes = (~ModeratorPermission,)
+        # Если действие на обновление или просмотр, то допускаем модератора
+        elif self.action in ['list', 'update', 'retrieve']:
+            self.permission_classes = (ModeratorPermission,)
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     """Класс для создания моделей уроков"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (~ModeratorPermission,)
 
     def perform_create(self, serializer):
         """Метод для автоматической привязки создающего пользователя к модели урок"""
@@ -39,21 +51,25 @@ class LessonListAPIView(ListAPIView):
     """Класс для вывода моделей уроков"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (ModeratorPermission,)
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     """Класс для вывода отдельной модели урока"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (ModeratorPermission,)
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     """Класс для редактирования моделей уроков"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (ModeratorPermission,)
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     """Класс для удаления моделей уроков"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (~ModeratorPermission,)
