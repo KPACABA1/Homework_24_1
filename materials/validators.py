5336fd18fa6cc213d652e9cd2c39a9f44df8c726
groni <smirnovapolina8200@mail.ru>
2024-10-07 16:41:55 +0500

    Поправил описание валидатора

--- materials/validators.py
+++ materials/validators.py
@@ -2,6 +2,6 @@ from rest_framework.serializers import ValidationError
 
 
 def validate_links(value):
-    """Делаю проверку, что начало ссылки начинается так, как ссылка из видео на YouTube"""
+    """Делаю проверку, что начало ссылки начинается так, как ссылка из видео на YouTube."""
     if not value[:24] == 'https://www.youtube.com/':
         raise ValidationError('Ссылка должна быть обязательно с YouTube')
